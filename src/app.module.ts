cd55ef4ca07035a1052a458492acffe68cba0fe5
Mahamadou-Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2023-11-14 20:45:15 +0100

    Add Environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,24 +4,22 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ApplicationModule } from './application/application.module';
 import { NoteModule } from './note/note.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://nouridine:5848094312@cluster0.bzxp8sd.mongodb.net/note-app?retryWrites=true&w=majority',
-      // 'mongodb://localhost:27017/test',
-      {
-        connectionFactory: (connection) => {
-          if (connection.readyState === 1) {
-            console.log('DB connected');
-          }
-          connection.on('disconnected', () => {
-            console.log('DB disconnected');
-          });
-          return connection;
-        },
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.ATLAS_MONGODB, {
+      connectionFactory: (connection) => {
+        if (connection.readyState === 1) {
+          console.log('DB connected');
+        }
+        connection.on('disconnected', () => {
+          console.log('DB disconnected');
+        });
+        return connection;
       },
-    ),
+    }),
     ApplicationModule,
     NoteModule,
   ],
